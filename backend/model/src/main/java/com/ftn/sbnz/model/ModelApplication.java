ff2677a1749440c111d5d33b1d57d55eef753e29
kaca01 <96604086+kaca01@users.noreply.github.com>
2024-05-11 15:18:22 +0200

    Project initialization

---
+++ backend/model/src/main/java/com/ftn/sbnz/model/ModelApplication.java
@@ -0,0 +1,11 @@
+package com.ftn.sbnz.model;
+
+import org.springframework.boot.SpringApplication;
+
+public class ModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelApplication.class, args);
+	}
+
+}
