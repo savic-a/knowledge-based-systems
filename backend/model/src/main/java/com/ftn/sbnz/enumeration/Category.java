8adb1a14f75a655b1e57a49fbb510d4e372bd299
savic-a <anastasijas557@gmail.com>
2024-05-28 19:38:20 +0200

    Finished rules

--- backend/model/src/main/java/com/ftn/sbnz/enumeration/Category.java
+++ backend/model/src/main/java/com/ftn/sbnz/enumeration/Category.java
@@ -1,11 +1,16 @@
 package com.ftn.sbnz.enumeration;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public enum Category {
+    
+    @Enumerated(EnumType.STRING)
     FOOD("Food"),
     ENTERTAINMENT("Entertainment"),
     SHOPPING("Shopping"),
--- backend/service/src/main/java/com/ftn/sbnz/service/services/rules/ForwardRulesService.java
+++ backend/service/src/main/java/com/ftn/sbnz/service/services/rules/ForwardRulesService.java
@@ -1,6 +1,7 @@
 package com.ftn.sbnz.service.services.rules;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 import org.kie.api.runtime.KieContainer;
@@ -24,23 +25,28 @@ public class ForwardRulesService {
 
     public void fireRules() {
         KieSession kSession = kieContainer.newKieSession("ksession-forward-1");
-        kSession.insert( new Client(1L, "PERA", "PERIC", "pera@gmail.com",  "123", false, false, false, false));
+        kSession.insert( new Client(1L, "PERA", "PERIC", "pera@gmail.com",  "123", new ArrayList<>(), new ArrayList<>(), false, false));
 
-        kSession.insert(new Transaction(1L, 50000, Timestamp.valueOf("2024-05-01 10:00:00"), Transaction.Type.INCOME, Category.FOOD, 1L));
-        kSession.insert(new Transaction(2L, 30000, Timestamp.valueOf("2024-05-05 12:00:00"), Transaction.Type.INCOME, Category.FOOD, 1L));
-        kSession.insert(new Transaction(3L, 70000, Timestamp.valueOf("2024-05-10 14:00:00"), Transaction.Type.INCOME, Category.FOOD, 1L));
+        kSession.insert(new Transaction(1L, 50000, Timestamp.valueOf("2024-05-01 10:00:00"), Transaction.Type.INCOME, Category.PRIVATE, 1L));
+        kSession.insert(new Transaction(2L, 30000, Timestamp.valueOf("2024-05-05 12:00:00"), Transaction.Type.INCOME, Category.PRIVATE, 1L));
+        kSession.insert(new Transaction(3L, 70000, Timestamp.valueOf("2024-05-10 14:00:00"), Transaction.Type.INCOME, Category.PRIVATE, 1L));
 
         // Create test transactions (shopping)
         kSession.insert(new Transaction(4L, 15000, Timestamp.valueOf("2024-05-12 10:00:00"), Transaction.Type.OUTCOME, Category.SHOPPING, 1L));
         kSession.insert(new Transaction(5L, 5000, Timestamp.valueOf("2024-05-13 12:00:00"), Transaction.Type.OUTCOME, Category.SHOPPING, 1L));
         kSession.insert(new Transaction(6L, 8000, Timestamp.valueOf("2024-05-14 14:00:00"), Transaction.Type.OUTCOME, Category.SHOPPING, 1L));
         kSession.insert(new Transaction(7L, 9000, Timestamp.valueOf("2024-05-15 16:00:00"), Transaction.Type.OUTCOME, Category.SHOPPING, 1L));
+        kSession.insert(new Transaction(8L, 9000, Timestamp.valueOf("2024-05-15 16:00:00"), Transaction.Type.OUTCOME, Category.SHOPPING, 1L));
 
         // Create test transactions (entertainment)
-        kSession.insert(new Transaction(8L, 5000, Timestamp.valueOf("2024-05-12 10:00:00"), Transaction.Type.OUTCOME, Category.ENTERTAINMENT, 1L));
-        kSession.insert(new Transaction(9L, 7000, Timestamp.valueOf("2024-05-13 12:00:00"), Transaction.Type.OUTCOME, Category.ENTERTAINMENT, 1L));
-        kSession.insert(new Transaction(10L, 6000, Timestamp.valueOf("2024-05-14 14:00:00"), Transaction.Type.OUTCOME, Category.ENTERTAINMENT, 1L));
+        kSession.insert(new Transaction(9L, 0, Timestamp.valueOf("2024-05-12 10:00:00"), Transaction.Type.OUTCOME, Category.ENTERTAINMENT, 1L));
+        kSession.insert(new Transaction(10L, 0, Timestamp.valueOf("2024-05-13 12:00:00"), Transaction.Type.OUTCOME, Category.ENTERTAINMENT, 1L));
+        kSession.insert(new Transaction(11L, 0, Timestamp.valueOf("2024-05-14 14:00:00"), Transaction.Type.OUTCOME, Category.ENTERTAINMENT, 1L));
 
+        kSession.insert(new Transaction(12L, 500, Timestamp.valueOf("2024-05-12 10:00:00"), Transaction.Type.OUTCOME, Category.FOOD, 1L));
+        kSession.insert(new Transaction(13L, 7000, Timestamp.valueOf("2024-05-13 12:00:00"), Transaction.Type.OUTCOME, Category.FOOD, 1L));
+        kSession.insert(new Transaction(14L, 6000, Timestamp.valueOf("2024-05-14 14:00:00"), Transaction.Type.OUTCOME, Category.FOOD, 1L));
+        kSession.insert(new Transaction(15L, 6000, Timestamp.valueOf("2024-05-14 14:00:00"), Transaction.Type.OUTCOME, Category.FOOD, 1L));
 
 
         kSession.fireAllRules();
