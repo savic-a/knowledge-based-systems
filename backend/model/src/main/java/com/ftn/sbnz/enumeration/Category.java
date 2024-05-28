7972d681dc6d16b001911370ce319fcf53619b69
savic-a <anastasijas557@gmail.com>
2024-05-28 14:03:09 +0200

    Implemented first rule

--- backend/model/src/main/java/com/ftn/sbnz/enumeration/Category.java
+++ backend/model/src/main/java/com/ftn/sbnz/enumeration/Category.java
@@ -11,7 +11,7 @@ public enum Category {
     SHOPPING("Shopping"),
     OUTINGS("Outings"),
     HEALTH_AND_CARE("Health and Care"),
-    PRIVATE("Private");
+    PRIVATE("Private");  // income transaction can have only this category
 
     private final String description;
 }
