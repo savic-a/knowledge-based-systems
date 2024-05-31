0e3091c77f31542217a78ee9c1986e235d616ef9
savic-a <anastasijas557@gmail.com>
2024-05-31 21:54:10 +0200

    Finished design

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -9,6 +9,16 @@ import FinancialGoal from './FinancialGoal';
 const Dashboard = () => {
   return (
     <div>
+      <div className="profile-info">
+        <img src="/images/profile.png" alt="Profile" style={{ width: '40px', height: '40px', borderRadius: '50%', marginRight: '10px' }} />
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <span style={{ margin: '0 5px 0 15px', fontWeight: 'normal' }}>Hello, </span> 
+            <span style={{ fontWeight: '500' }}>Pera</span>
+          </div>
+          <div style={{ fontWeight: 'normal', fontSize: '12px', color:"gray", marginLeft: "15px" }}>Let's manage your wallet finance</div>
+        </div>
+      </div>
       <Navigation />
       <div className="main-content">
         <div className="left-side">
