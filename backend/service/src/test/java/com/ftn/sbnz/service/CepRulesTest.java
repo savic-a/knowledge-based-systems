d737cf7d69774d1362b5e213f17bc57fad105ff9
savic-a <anastasijas557@gmail.com>
2024-06-04 13:14:12 +0200

    Created test for cep

---
+++ backend/service/src/test/java/com/ftn/sbnz/service/CepRulesTest.java
@@ -0,0 +1,58 @@
+package com.ftn.sbnz.service;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+import org.junit.Test;
+import org.kie.api.KieServices;
+import org.kie.api.event.rule.DebugAgendaEventListener;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+import com.ftn.sbnz.event.TransactionEvent;
+import com.ftn.sbnz.model.Budget;
+import com.ftn.sbnz.model.Client;
+import com.ftn.sbnz.model.FinancialGoal;
+
+public class CepRulesTest {
+    
+    @Test
+    public void testCEPRules() {
+    	KieServices ks = KieServices.Factory.get();
+    	KieContainer kc = ks.newKieClasspathContainer();
+        KieSession ksession = kc.newKieSession("ksession-cep");
+
+        // Insert client and budget
+        Long clientId = 1L;
+        ksession.insert(new Client(clientId, "PERA", "PERIC", "pera@gmail.com", "123", new ArrayList<>(), new ArrayList<>(), false, false));
+        ksession.insert(new Budget(clientId, 100000, clientId));
+
+        // Insert financial goal
+        ksession.insert(new FinancialGoal(clientId, "Savings Goal", "Save money for a vacation", 
+                Timestamp.valueOf("2024-01-01 00:00:00"), 100000, Timestamp.valueOf("2024-12-31 23:59:59"), 120000, 50000, clientId));
+
+        // Insert transaction events
+        ksession.insert(new TransactionEvent(clientId, 25000, TransactionEvent.Type.OUTCOME, clientId));
+        ksession.insert(new TransactionEvent(clientId, 5000, TransactionEvent.Type.OUTCOME, clientId));
+        ksession.insert(new TransactionEvent(clientId, 60000, TransactionEvent.Type.OUTCOME, clientId));
+        ksession.insert(new TransactionEvent(clientId, 50000, TransactionEvent.Type.OUTCOME, clientId));
+        ksession.insert(new TransactionEvent(clientId, 120000, TransactionEvent.Type.OUTCOME, clientId));
+
+        // Fire all rules
+        long ruleFireCount = ksession.fireAllRules();
+        System.out.println("Rules fired: " + ruleFireCount);
+
+        // Insert additional transaction event to trigger more rules
+        ksession.insert(new TransactionEvent(clientId, 202000, TransactionEvent.Type.OUTCOME, clientId));
+        ruleFireCount = ksession.fireAllRules();
+        System.out.println("Rules fired: " + ruleFireCount);
+        
+        // Check the results
+        // for (Object obj : ksession.getObjects()) {
+        //     if (obj instanceof AnalysisTransaction) {
+        //         System.out.println(obj);
+        //     }
+        // }
+
+    }
+}
