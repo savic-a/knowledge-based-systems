562dd1f4c76039a48cc0840a9e43b9f3111a776d
kaca01 <96604086+kaca01@users.noreply.github.com>
2024-06-10 02:02:51 +0200

    Forward first level

---
+++ backend/model/src/main/java/com/ftn/sbnz/enumeration/PurchaseType.java
@@ -0,0 +1,18 @@
+package com.ftn.sbnz.enumeration;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PurchaseType {
+
+    @Enumerated(EnumType.STRING)
+    IMPULSIVE("Impulsive"),
+    Frequent("Frequent");
+    
+    private final String description;
+}
---
+++ backend/service/src/test/java/com/ftn/sbnz/service/ForwardRulesTest.java
@@ -0,0 +1,38 @@
+package com.ftn.sbnz.service;
+
+import java.sql.Timestamp;
+
+import org.junit.Test;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.KieSessionConfiguration;
+
+import com.ftn.sbnz.enumeration.Category;
+import com.ftn.sbnz.model.Budget;
+import com.ftn.sbnz.model.BudgetExceeding;
+import com.ftn.sbnz.model.Transaction;
+
+public class ForwardRulesTest {
+
+    @Test
+    public void forwardRulesTest() {
+        KieServices ks = KieServices.Factory.get();
+    	KieContainer kc = ks.newKieClasspathContainer();
+
+        KieSessionConfiguration config = KieServices.Factory.get().newKieSessionConfiguration();
+        KieSession ksession = kc.newKieSession("ksession-forward-1", config);
+        ksession.setGlobal("highestCategoryCost", 0.0);
+        ksession.setGlobal("highestCategory", null);
+        ksession.setGlobal("highestCategoryCount", 0);
+        ksession.setGlobal("numOfCategories", 6);
+        
+        ksession.insert(new Budget(1L, 50000, 1L));
+        ksession.insert(new Transaction(6L, 40000, Timestamp.valueOf("2024-01-01 00:00:00"), Transaction.Type.OUTCOME, 1L, Category.HEALTH_AND_CARE));
+        ksession.insert(new Transaction(7L, 30000, Timestamp.valueOf("2024-01-01 00:00:00"), Transaction.Type.OUTCOME, 1L, Category.FOOD));
+        ksession.insert(new Transaction(8L, 15000, Timestamp.valueOf("2024-01-01 00:00:00"), Transaction.Type.OUTCOME, 1L, Category.FOOD));
+        ksession.insert(new BudgetExceeding(1L));
+        ksession.fireAllRules();
+    }
+    
+}
