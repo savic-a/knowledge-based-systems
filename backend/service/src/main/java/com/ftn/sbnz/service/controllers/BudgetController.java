8f198a3477ea376bf0f2387c488d42169835f229
savic-a <anastasijas557@gmail.com>
2024-06-11 17:14:13 +0200

    Added endpoints for budget

--- backend/service/src/main/java/com/ftn/sbnz/service/controllers/BudgetController.java
+++ backend/service/src/main/java/com/ftn/sbnz/service/controllers/BudgetController.java
@@ -6,10 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ftn.sbnz.dto.BudgetDTO;
+import com.ftn.sbnz.dto.ReportDTO;
 import com.ftn.sbnz.model.Budget;
+import com.ftn.sbnz.model.CreditCard;
+import com.ftn.sbnz.model.Report;
 import com.ftn.sbnz.service.services.implementations.BudgetService;
 import com.ftn.sbnz.service.services.interfaces.IService;
 
@@ -18,6 +25,9 @@ import com.ftn.sbnz.service.services.interfaces.IService;
 public class BudgetController {
 
     private IService<Budget> service;
+
+    @Autowired
+    BudgetService budgetService;
 
     @Autowired
     public BudgetController(BudgetService service) {
@@ -30,4 +40,14 @@ public class BudgetController {
         return new ResponseEntity<>(budgets, HttpStatus.OK);
     }
     
+    @GetMapping("/{clientId}")
+    public Budget getBudgetByClientId(@PathVariable Long clientId) {
+        return this.budgetService.getBudgetByClientId(clientId);
+    }
+
+    @PostMapping("/{clientId}")
+    public ResponseEntity<Budget> addBudget(@PathVariable Long clientId, @RequestBody BudgetDTO request) {
+        Budget budget = this.budgetService.addBudget(clientId, request);
+        return new ResponseEntity<Budget>(budget, HttpStatus.OK);
+    }
 }
