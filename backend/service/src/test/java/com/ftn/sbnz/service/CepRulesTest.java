fea2ea391c0b9d4791b215bfe9c4ebf78fad0b9b
savic-a <anastasijas557@gmail.com>
2024-06-11 16:08:37 +0200

    Removed warning and transactionEvent

--- backend/service/src/test/java/com/ftn/sbnz/service/CepRulesTest.java
+++ backend/service/src/test/java/com/ftn/sbnz/service/CepRulesTest.java
@@ -3,7 +3,7 @@ package com.ftn.sbnz.service;
 import org.junit.Test;
 
 import java.sql.Timestamp;
-import java.util.ArrayList;
+import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
 
 import org.kie.api.KieServices;
@@ -13,10 +13,11 @@ import org.kie.api.runtime.KieSessionConfiguration;
 import org.kie.api.runtime.conf.ClockTypeOption;
 import org.kie.api.time.SessionPseudoClock;
 
-import com.ftn.sbnz.event.TransactionEvent;
+import com.ftn.sbnz.enumeration.Category;
 import com.ftn.sbnz.model.Budget;
 import com.ftn.sbnz.model.Client;
 import com.ftn.sbnz.model.FinancialGoal;
+import com.ftn.sbnz.model.Transaction;
 
 public class CepRulesTest {
     
@@ -45,18 +46,18 @@ public class CepRulesTest {
       ksession.insert(new FinancialGoal(3L, "Savings Goal", "Save money for a vacation", Timestamp.valueOf("2024-01-01 00:00:00"), 100000, Timestamp.valueOf("2024-12-31 23:59:59"), 120000, 10000, 3L));
        
       // Simulate first week - budget overrun
-       ksession.insert(new TransactionEvent(1L, 120000, TransactionEvent.Type.OUTCOME, 1L)); // Exceeds budget
+       ksession.insert(new Transaction(1L, 120000, Timestamp.valueOf(LocalDateTime.now()), Transaction.Type.OUTCOME, Category.FOOD, 1L)); // Exceeds budget
        ksession.fireAllRules();
        clock.advanceTime(7, TimeUnit.DAYS); // Move clock forward by one week
 
        // Simulate second week - sudden jump in costs
-       ksession.insert(new TransactionEvent(2L, 5000, TransactionEvent.Type.OUTCOME, 2L));
-       ksession.insert(new TransactionEvent(3L, 50000, TransactionEvent.Type.OUTCOME, 2L)); // Sudden jump in cost
+       ksession.insert(new Transaction(2L, 5000, Timestamp.valueOf(LocalDateTime.now()), Transaction.Type.OUTCOME, Category.ENTERTAINMENT, 2L));
+       ksession.insert(new Transaction(3L, 50000, Timestamp.valueOf(LocalDateTime.now()), Transaction.Type.OUTCOME, Category.ENTERTAINMENT, 2L)); // Sudden jump in cost
        ksession.fireAllRules();
        clock.advanceTime(7, TimeUnit.DAYS); // Move clock forward by one week
 
        // Simulate third week - financial goal achieved
-       ksession.insert(new TransactionEvent(4L, 20000, TransactionEvent.Type.OUTCOME, 3L)); // Achieve financial goal
+       ksession.insert(new Transaction(4L, 20000, Timestamp.valueOf(LocalDateTime.now()), Transaction.Type.OUTCOME, Category.SHOPPING, 3L)); // Achieve financial goal
        ksession.fireAllRules();
        clock.advanceTime(7, TimeUnit.DAYS); // Move clock forward by one week
 
